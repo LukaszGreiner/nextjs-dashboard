762dccd423fa8cfda11b47b0803e04a7c9b6239d
xKaktus <greinerlukasz@gmail.com>
2025-11-30 12:25:07 +0100

    Start chapter 12

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,12 +2,12 @@ import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData } from "../lib/data";
+import { fetchCardData } from "../../lib/data";
 import {
   CardsSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
-} from "../ui/skeletons";
+} from "../../ui/skeletons";
 import { Suspense } from "react";
 
 export default async function Page() {
